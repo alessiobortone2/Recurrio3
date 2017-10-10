ac1a4646ad0eb2353fcb2b40749ed8c16cb1ed9f
Denis <denisglb@yahoo.co.uk>
2017-10-10 11:03:39 +0100

    Homepage view

--- Hardware/ViewController.swift
+++ Hardware/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let cellContent = ["Water", "Hendricks", "Milk", "Mayonnaise", "Almonds"]
+    
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cellContent.count
+    }
+    
+    
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = cellContent[indexPath.row]
+        
+        let image = UIImage(named: "food")
+        cell.imageView!.image = image
+//        let button =  ButtonMenu
 
+        return cell
+    }
+    
+    @IBOutlet weak var ButtonMenu: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
+}
