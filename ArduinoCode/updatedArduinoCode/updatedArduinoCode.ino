69e95602dd3bd89568b1772f25765845ea419424
Will Porter <willjsporter@hotmail.co.uk>
2017-10-19 12:27:00 +0100

    arduino code now verified

--- ArduinoCode/updatedArduinoCode/updatedArduinoCode.ino
+++ ArduinoCode/updatedArduinoCode/updatedArduinoCode.ino
@@ -1,32 +1,30 @@
 int fsrPin = 0;     // the FSR and 10K pulldown are connected to a0
 int fsrReading;     // the analog reading from the FSR resistor divider
 int ledPin = 13;
-int fsrReading;
 int averageReadings[10];
 int arrayIndex = 0;
 int currentWeightReading;
 int arrayTotal;
-  for (int initialValues = 0; initialValues < 10; initialValues ++) {
-    averageReadings[initialValues] = 0;
-  }
+
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.begin(9600);   
   pinMode(ledPin, OUTPUT);
-  
+  for (int initialValues = 0; initialValues < 10; initialValues ++) {
+    averageReadings[initialValues] = 0;
+    }
 }
 
 void loop() {
   fsrReading = analogRead(fsrPin); 
   if (fsrReading != 0 ) {
-    arrayTotal -= averageReadings[arrayIndex]
+    arrayTotal -= averageReadings[arrayIndex];
     averageReadings[arrayIndex] = fsrReading;
-    arrayTotal += averageReadings[arrayIndex]
+    arrayTotal += averageReadings[arrayIndex];
     arrayIndex ++;
     currentWeightReading = arrayTotal/10;
-    Serial.println(currentWeightReading)
+    Serial.println(currentWeightReading);
   }
 }
